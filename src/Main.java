87957178edaf3a4f3cc25750f1b5cfe693d0d84f
Flagn3 <flagn3@gmail.com>
2025-02-05 10:29:20 +0100

    Añadir lista a la clase Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
-
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hola");
+        System.out.printf("Hola\n");
+
+        List<Integer> lista=new ArrayList<>();
+        lista.add(8);
+        lista.add(16);
+        lista.add(1);
+        lista.add(3);
+
+        System.out.println(lista.toString());
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
